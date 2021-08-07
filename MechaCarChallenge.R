9572f0bc03efd0a43bdac35ae9713108a97c9c0e
Tommy Williams <tmmytwllms@gmail.com>
2021-08-07 19:55:37 -0400

    Created summary statistics for the suspension_coil csv, and also drilled down into summary statistics by manufacturing lot.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,15 @@
 library(dplyr) #import deplyr library
 
-mecha_car_df <- read.csv(file="MechaCar_mpg.csv",stringsAsFactors=F) #read the csv into a data frame
+mecha_car_df <- read.csv(file="MechaCar_mpg.csv",stringsAsFactors=F) #read the mechacar_mpg csv into a data frame
 
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,data=mecha_car_df) #perform linear regression for mpg
 
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD+mpg,data=mecha_car_df))
 #determine p-value and r-squared value
+
+suspension_coil_df <- read.csv(file="Suspension_Coil.csv",stringsAsFactors=F) #read the suspension_coil csv into a data frame
+
+total_summary <- suspension_coil_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI))
+#create a summary data frame to show mean, median, variance, and standard deviation for PSI
+
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI))
